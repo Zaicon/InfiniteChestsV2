47f3244a226e7ff74109e3e1f9d4af81d1e43e1a
Zaicon Kiroshu <kzaicon@yahoo.com>
2016-12-07 09:19:37 -0600

    Mintaka Pre-Release Build

--- InfChests/InfChests.cs
+++ InfChests/InfChests.cs
@@ -8,6 +8,7 @@ using Terraria.IO;
 using TerrariaApi.Server;
 using TShockAPI;
 using System.Linq;
+using Microsoft.Xna.Framework;
 
 namespace InfChests
 {
@@ -358,7 +359,7 @@ namespace InfChests
 
 		private bool getChestContents(int index, short tilex, short tiley)
 		{
-			string filepath = "chestdebug.txt";
+			//string filepath = "chestdebug.txt";
 
 			InfChest chest = DB.getChest(tilex, tiley);
 			TSPlayer player = TShock.Players[index];
@@ -1027,7 +1028,7 @@ namespace InfChests
 		{
 			if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "confirm")
 			{
-				DB.ConvertRefills();
+				await Task.Factory.StartNew(() => DB.ConvertRefills());
 				args.Player.SendSuccessMessage("Converted all chests with a refill time of '0' to '-1'!");
 
 				Commands.ChatCommands.RemoveAll(e => e.Name == "fixchests");
